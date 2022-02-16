e3c78049369f38408d401b2947b6294b87b6875a
Lautaro <lautigle2013@gmail.com>
2022-02-16 12:29:36 -0300

    beauty commit

--- challenge.js
+++ challenge.js
@@ -14,40 +14,39 @@ class Stock {
     }
 
     addStockHist(date, price) {
-        this.stockHist.push({date, price})
+        this.stockHist.push({ date, price });
     }
 
     getPrice(date) {
-        const stock = this.stockHist.find( e => e.date.getTime() == date.getTime());
-        return stock.price
+        const stock = this.stockHist.find( e => e.date.getTime() == date.getTime() );
+        return stock.price;
     }
 }
 
 class Portfolio {
     constructor(stock) {
-        this.stock = stock
+        this.stock = stock;
     }
 
     // Cumulative Return = (Last price / Initial price - 1)
     cumulativeReturn( finalPrice, initPrice) {
-        return (finalPrice / initPrice - 1)
+        return (finalPrice / initPrice - 1);
     }
 
     // Annualized Return  = (1 + Cumulative Return) ** 365 / DaysHeld - 1 
     getProfit(finalDate, initDate) {
-        const cumulativeReturn = this.cumulativeReturn( this.stock.getPrice(finalDate), this.stock.getPrice(initDate) )
-        const DaysHeld = Math.ceil((finalDate.getTime() - initDate.getTime()) / (1000 * 3600 * 24))
-        const annualizedReturn = (1 + cumulativeReturn) ** (365 / DaysHeld) - 1
-        return annualizedReturn
+        const cumulativeReturn = this.cumulativeReturn( this.stock.getPrice( finalDate ), this.stock.getPrice( initDate ) );
+        // ( 1000 miliseconds * 3600 seconds (60 mins * 60 seconds) * 24 hours ) it gives me the total quantity of miliseconds on a day.
+        const DaysHeld = Math.ceil(( finalDate.getTime() - initDate.getTime() ) / ( 1000 * 3600 * 24 ) );
+        const annualizedReturn = (1 + cumulativeReturn) ** (365 / DaysHeld) - 1;
+        return annualizedReturn;
     }
 }
 
-
-
-const stock = new Stock('Fintual')
+const stock = new Stock('Fintual');
 stock.addStockHist(new Date('2021-09-15'), 10);
 stock.addStockHist(new Date('2022-02-14'), 100);
 
 
 const portfolio = new Portfolio(stock);
-console.log(portfolio.getProfit(new Date('2022-02-14'), new Date('2021-09-15')))
+console.log(portfolio.getProfit(new Date('2022-02-14'), new Date('2021-09-15')));
